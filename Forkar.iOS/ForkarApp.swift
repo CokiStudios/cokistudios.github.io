4198b1578ece79c71d8dc7ddde6bd775d15c1b5c
anonymus-devop <jerixortixdev@gmail.com>
2026-09-20 21:31:34 -0500

    feat(firebase-ios): configurar inicializacion de FirebaseApp con AppDelegate en ForkarApp.swift

--- Forkar.iOS/ForkarApp.swift
+++ Forkar.iOS/ForkarApp.swift
@@ -1,10 +1,26 @@
 import SwiftUI
+#if canImport(FirebaseCore)
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+#endif
 
 @main
 struct ForkarApp: App {
+#if canImport(FirebaseCore)
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+#endif
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
